a406c7352306a4997971b5d98644744a87b6a72d
Miguel Diaz L <miguel.diaz@mdiazlira.com>
2015-05-03 15:52:57 -0400

    Script 2 Ready

--- script_2.py
+++ script_2.py
@@ -1,23 +1,63 @@
 import os
 import nltk
+from nltk.corpus import stopwords
 
-
+# This method is usted to split the title, author and abstract from a series of
+# well-formed-texts. A Well-formed-texts follows the following structure:
+#	title;;;author;;;abstract
+#
+# Return 3-tuple -> (title, author, abstract)
 def split_text_data(data):
 	separator = ";;;"
 	try:
 		return data.split(separator)
 	except:
 		return None
-		
+
+# This method is used to get Sentences from a particular text
+#
+# Return: Sentences found on text
 def get_sentencias(texto):
 	sentencias = list()
 	sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 	return sent_tokenizer.tokenize(texto) 
 
-def process_sentencia(sentencia):
-	
+# This method is used to get Tokens from a particular sentence
+# The process is as follows:
+#	1.- Lowercase everything
+#	2.- Remove Punctuations and Some Latex Symbols
+#	3.- Remove Stopwords from English dictionary (NLTK)
+#	4.- Lemmatize tokens found on sentences
+#
+# Return: List of unigrams from sentence.
+def get_tokens(sentencia):
+	# To Lower
+	sentencia = sentencia.lower()
 
-def test():
+	# Remove Punctuation and Latex Stuff
+	puncts = ",;.$\\\\"
+	for sym in puncts:
+		sentencia = sentencia.replace(sym," ")
+
+	# Tokenize
+	sentencia.strip()
+	tokens = nltk.word_tokenize(sentencia)
+
+	# Removing Stopwords and latex stuff
+	stop = stopwords.words("english")
+	stop_latex = ["$","\\"]
+	stop.extend(stop_latex)
+	tokens = [token.lower() for token in tokens if token not in stop]
+
+	# Wordnet Lematization
+	wnl = nltk.WordNetLemmatizer()
+	tokens = [wnl.lemmatize(token) for token in tokens]
+
+	return tokens
+
+
+# Script to test the methods...
+def script_2():
 	# Abstract analized list
 	abstract_list = list() 
 	processed = 0
@@ -35,9 +75,20 @@ def test():
 
 			# If data is correct (well-formed-texts)
 			if len(text_data) == 3:
+				# Debug Texts
+				#print "Processing: {0}".format(file)
+				#print "-------------------"
+
 				# Get sentences
 				sentences = get_sentencias(text_data[2])
-				print(sentences)
+				
+				# Debug Sentences
+				#for s in sentences:
+				#	print "* {0}".format(s)
+
+				# Get unigramas based on tokens
+				for sentence in sentences:
+					unigramas = get_tokens(sentence)
 
 				# Count books
 				processed += 1
@@ -46,4 +97,4 @@ def test():
 
 	print("$ {0} Abstracts processed. {1} corrupted.".format(processed, corrupted))
 
-test()
+script_2()
