ac5b2f8e9b67bf735b765ae22da24a33b73e90e3
Leo137 <leo.psicokinesis@gmail.com>
2015-05-03 13:50:48 -0400

    lee y escribe vocabulario

--- script_1.py
+++ script_1.py
@@ -1,7 +1,22 @@
 ﻿import os
+import nltk
+from nltk.corpus import stopwords
+
+vocab = list()
 for file in os.listdir("abstracts"):
     if file.endswith(".txt"):
         f = open("abstracts/" + file,"r")
         file_text = f.read()
-        # print(file_text)
-        print(file)
+        tokens = nltk.word_tokenize(file_text)
+        text = nltk.Text(tokens)
+        # collocations = text.collocations() # detectar collocations
+        words = [w.lower() for w in text]
+        vocab.extend(sorted(set(words))) # agregar a vocabulario 
+puncts = ".,?!:;{}[]()<>"
+for sym in puncts:
+    vocab = [item for item in vocab if item != sym]
+vocab = sorted(set(vocab))
+v = open("vocab.txt","w")
+for element in vocab:
+    v.write("'" + element + "'" + ",")
+v.close()
