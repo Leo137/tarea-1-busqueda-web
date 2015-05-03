1b873c71d4a35f661c692746c5b0bf0ac25b6192
Leo137 <leo.psicokinesis@gmail.com>
2015-05-03 17:49:00 -0400

    creacion de inverted index

--- script_1.py
+++ script_1.py
@@ -1,7 +1,7 @@
 ﻿import os
 import nltk
-from nltk.corpus import stopwords
-from nltk.collocations import *
+from pprint import pprint as pp
+import script_4
 
 def get_processed_text():
     text = list()
@@ -18,7 +18,7 @@ def get_processed_text():
             tokens = (nltk.word_tokenize(file_text))
             
             # Stopwords detection
-            stop = stopwords.words("english") 
+            stop = nltk.corpus.stopwords.words("english") 
             filtered = [token.lower() for token in tokens if token not in stop]
             
             # Add to processed text list
@@ -34,7 +34,7 @@ def find_bigrams(text):
     # Bigram measures
     bigram_measures = nltk.collocations.BigramAssocMeasures()
     
-    finder = BigramCollocationFinder.from_words(text)
+    finder = nltk.collocations.BigramCollocationFinder.from_words(text)
     finder.apply_freq_filter(3)
     return finder.nbest(bigram_measures.pmi, 10*len(os.listdir("abstracts"))) # detectar collocations
     
@@ -43,6 +43,45 @@ def get_vocabulary_from_collocations(bigrams):
     words = [(w[0] + " " + w[1]).lower() for w in bigrams]
     vocab.extend(sorted(set(words))) # agregar a vocabulario 
     return sorted(set(vocab))
+    
+def create_inverted_index(collocations):
+
+    inverted_index = {}
+    # Open ALL abstracts
+    for file in os.listdir("abstracts"):
+        if file.endswith(".txt"):
+        
+            # Read Text
+            f = open("abstracts/" + file,"r")
+            file_text = f.read()
+            
+            # Get tokens
+            tokens = (nltk.word_tokenize(file_text))
+            
+            # Stopwords detection
+            stop = nltk.corpus.stopwords.words("english") 
+            filtered = [token.lower() for token in tokens if token not in stop]
+            
+            # Generate processed text
+            text = nltk.Text(filtered)
+            
+            # Remove possible punctuation marks
+            puncts = ".,?!:;{}[]()<>$'"
+            for sym in puncts:
+                text  = [item for item in text if item != sym]
+            
+            # Get bigrams
+            b = nltk.bigrams(text)
+            
+            # Find matches and add them to the inverted index
+            for match in (set(collocations) & set(b)):
+                value = match[0] + " " + match[1]
+                if value in inverted_index:
+                    inverted_index[value].append(file)
+                else:
+                    inverted_index[value] = [file]
+    
+    return inverted_index
     
 def script_1():
 
@@ -59,8 +98,11 @@ def script_1():
     v = open("vocab.txt","w")
     for element in vocab:
         v.write("'" + element + "'" + ",")
+        
     v.close()
     
     print("$ {0} Collocations founded, saved in vocab.txt".format(len(bigrams)))
+    
+    script_4.script_4(bigrams)
 
 script_1()
---
+++ script_4.py
@@ -0,0 +1,57 @@
+﻿import os
+import nltk
+
+def create_inverted_index(collocations):
+
+    inverted_index = {}
+    # Open ALL abstracts
+    for file in os.listdir("abstracts"):
+        if file.endswith(".txt"):
+        
+            # Read Text
+            f = open("abstracts/" + file,"r")
+            file_text = f.read()
+            
+            # Get tokens
+            tokens = (nltk.word_tokenize(file_text))
+            
+            # Stopwords detection
+            stop = nltk.corpus.stopwords.words("english") 
+            filtered = [token.lower() for token in tokens if token not in stop]
+            
+            # Generate processed text
+            text = nltk.Text(filtered)
+            
+            # Remove possible punctuation marks
+            puncts = ".,?!:;{}[]()<>$'"
+            for sym in puncts:
+                text  = [item for item in text if item != sym]
+            
+            # Get bigrams
+            b = nltk.bigrams(text)
+            
+            # Find matches and add them to the inverted index
+            for match in (set(collocations) & set(b)):
+                value = match[0] + " " + match[1]
+                if value in inverted_index:
+                    inverted_index[value].append(file)
+                else:
+                    inverted_index[value] = [file]
+    
+    return inverted_index
+    
+def script_4(bigrams):
+
+    # Get the inverted index
+    inverted_index = create_inverted_index(bigrams)
+    
+    # Write to file
+    i = open("inverted.txt","w")
+    for k,v in inverted_index.items():
+        i.write("'" + k + "' : [")
+        for item in v:
+            i.write("'" + item + "' ")
+        i.write("]\n")
+    i.close()
+    
+    print("$ {0} keys founded, saved inverted index to inverted.txt".format(len(inverted_index)))
