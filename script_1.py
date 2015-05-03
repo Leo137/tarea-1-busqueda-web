e2306b045b1f373ae41072faad0bb75705ad6b3c
Leo137 <leo.psicokinesis@gmail.com>
2015-05-03 12:46:49 -0400

    script basico

---
+++ script_1.py
@@ -0,0 +1,7 @@
+﻿import os
+for file in os.listdir("abstracts"):
+    if file.endswith(".txt"):
+        f = open("abstracts/" + file,"r")
+        file_text = f.read()
+        # print(file_text)
+        print(file)
